d835b1d6c76b2926cb11c574edd41eec3222e8d5
magdballol-afk <magdballol@gmail.com>
2026-09-04 04:09:23 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,9 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        
-        versionCode = flutterVersionCode.toInt()
-versionName = flutterVersionName
+        versionCode = flutter.versionCode
+versionName = flutter.versionName
 
     }
 
